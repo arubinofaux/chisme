16b71eec07136c3d901f22759d75fbf3201d6382
arubinofaux <arubinofaux@gmail.com>
2019-11-23 17:32:56 -0500

    wip

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
 
 const Plate = models.Plate
 const Seen = models.Seen
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.post('/push', function (req, res) {
   if (req.body.data_type == "alpr_results") {
@@ -17,16 +23,14 @@ app.post('/push', function (req, res) {
 
     Plate.findOne({ where: { number: results.plate } }).then(plate => {
       if (plate) {
-        if (results.confidence > 89) {
-          Seen.create({
-            uuid: data.uuid,
-            confidence: results.confidence,
-            processing_time_ms: results.processing_time_ms,
-            PlateId: plate.get('id')
-          })
-        }
+        Seen.create({
+          uuid: data.uuid,
+          confidence: results.confidence,
+          processing_time_ms: results.processing_time_ms,
+          PlateId: plate.get('id')
+        })
       } else {
-        if (results.confidence > 89) {
+        if (results.confidence > 85) {
           Plate.create({
             number: results.plate,
             seens: [
@@ -39,6 +43,13 @@ app.post('/push', function (req, res) {
           }, {
             include: [{model: Seen, as: 'seens'}]
           });
+
+          io.emit('new plate', { 
+            plate: results.plate, 
+            uuid: data.uuid,
+            confidence: results.confidence,
+            image: "http://chismiando.local/"+data.uuid+".jpg"
+          });
         }
       }
     })
@@ -47,11 +58,19 @@ app.post('/push', function (req, res) {
   res.json({ status: 'chill' })
 })
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  socket.on('new plate', function(msg){
+    console.log(msg);
+  });
+});
+
 models.sequelize.sync().then(function() {
   /**
    * Listen on provided port, on all network interfaces.
    */
-  app.listen(3000, function() {
+  http.listen(3000, function() {
     console.log('Express en el chisme');
   });
 });
